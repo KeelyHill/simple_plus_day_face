9ee7e18c9106970ca3265fde77bea3446c299e97
Keely Hill <keelyman@gmail.com>
2015-08-06 11:53:43 -0400

    Fixed no date num showing in August
    Also made small changes to where each elements bounds end using the window width - 15

--- src/simplicity.c
+++ src/simplicity.c
@@ -20,9 +20,9 @@ static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   // Need to be static because they're used by the system later.
   static char s_time_text[] = "00:00";
   static char s_date_text[] = "Month 00";
-  static char s_day_text[] = "Day";
+  static char s_day_text[] = "day";
 
-  strftime(s_date_text, sizeof(s_date_text), "%B %e", tick_time);
+  strftime(s_date_text, sizeof(s_date_text), "%B%e", tick_time);
   text_layer_set_text(s_date_layer, s_date_text);
   
   strftime(s_day_text, sizeof(s_day_text), "%a", tick_time);
@@ -45,18 +45,20 @@ static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
   
   #define UP 50
+  #define END_WIDTH window_bounds.size.w - 15
 
   // Date Layer (Month 00)
-  s_date_layer = text_layer_create(GRect(8, UP, 136, 100));
+  s_date_layer = text_layer_create(GRect(5, UP, END_WIDTH, 50));
   text_layer_set_text_color(s_date_layer, FRONT_COLOR);
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
   
   // Day Layer (Sun-Sat)
-  s_day_layer = text_layer_create(GRect(7, UP, 130, 50));
+  s_day_layer = text_layer_create(GRect(5, UP, END_WIDTH, 50));
   text_layer_set_text_color(s_day_layer, FRONT_COLOR);
   text_layer_set_background_color(s_day_layer, GColorClear);
   text_layer_set_text_alignment(s_day_layer, GTextAlignmentRight);
@@ -64,17 +66,18 @@ static void main_window_load(Window *window) {
   layer_add_child(window_layer, text_layer_get_layer(s_day_layer));
   
   // Time Layer
-  s_time_layer = text_layer_create(GRect(7, 24 + UP, 137, 76));
+  s_time_layer = text_layer_create(GRect(5, 24 + UP, END_WIDTH, 76));
   text_layer_set_text_color(s_time_layer, FRONT_COLOR);
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
-  GRect line_frame = GRect(7, 29 + UP, 130, 2);
+  GRect line_frame = GRect(5, 29 + UP, END_WIDTH, 2);
   s_line_layer = layer_create(line_frame);
   layer_set_update_proc(s_line_layer, line_layer_update_callback);
   layer_add_child(window_layer, s_line_layer);
 }
+
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_date_layer);
